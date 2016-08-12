ab350d6b2cd972c628e0d16391a3bdd259706113
Olivier Biesmans <olivier.biesmans@blablacar.com>
2016-08-12 11:14:56 +0200

    Start PEP8 compliance... good luck :)

--- zbx.py
+++ zbx.py
@@ -25,19 +25,22 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Import or use the embed version of fail with a correct warning msg
 try:
-	import click
+    import click
 except ImportError:
-    sys.exit("You need Click! install it from https://github.com/pallets/click or run : sudo pip3 install click.")
+    sys.exit("""
+You need Click! install it from https://github.com/pallets/click"
+or run : sudo pip3 install click."
+""")
 
 try:
-	import requests
+    import requests
 except ImportError:
     sys.exit("You need requests! install it from https://github.com/kennethreitz/requests or run : sudo pip3 install requests.")
 
 # Force lib version embedded
 # Because tested or patched !
 vendor_dir = os.path.join(BASE_DIR, 'vendor/python')
-sys.path.insert(1,vendor_dir)
+sys.path.insert(1, vendor_dir)
 from pyzabbix import ZabbixAPI
 from tabulate import tabulate
 
@@ -50,7 +53,7 @@ ZABBIX_USER = config['zabbix']['user']
 ZABBIX_PASSWORD = config['zabbix']['password']
 
 # Color stuff
-eCOLOR_NONE="\033[0;39m"
+eCOLOR_NONE = "\033[0;39m"
 eLIGHT_RED="\033[1;31m"
 eLIGHT_GREEN="\033[1;32m"
 Bold="\033[1m"
@@ -65,83 +68,83 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version='1.0.0')
 def zabbix():
-	"""This is a Zabbix user command line tools."""
-	pass
+    """This is a Zabbix user command line tools."""
+    pass
 
 @zabbix.group()
 def maintenance ():
-	"""maintenances management sub-commands"""
-	pass
+    """maintenances management sub-commands"""
+    pass
 
 ##########################################################################
 # FUNCTIONS
 ##########################################################################
 
 def get_host_id(fqdn):
-	response = zapi.host.get(
-		output='extend',
-		filter={"host": fqdn}
-		)
-	if not response:
-		print("Host not found in Zabbix : %s" % fqdn)
-		sys.exit(42)
-	else:
-		result = response[0]["hostid"]
-		return result
+    response = zapi.host.get(
+        output='extend',
+        filter={"host": fqdn}
+        )
+    if not response:
+        print("Host not found in Zabbix : %s" % fqdn)
+        sys.exit(42)
+    else:
+        result = response[0]["hostid"]
+        return result
 
 def get_tempate_id(template_name):
-	response = zapi.template.get(
-		output='extend',
+    response = zapi.template.get(
+        output='extend',
         filter={"host":"Template OS Linux"}
         )
-	if not response:
-		print("Template not found in Zabbix : %s" % template_name)
-		sys.exit(42)
-	else:
-		result = response[0]["templateid"]
-		return result
+    if not response:
+        print("Template not found in Zabbix : %s" % template_name)
+        sys.exit(42)
+    else:
+        result = response[0]["templateid"]
+        return result
 
 def to_date(ts):
     return datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S')
 
 def get_maintenance_id(host_id):
-	response = zapi.maintenance.get(
-		output='extend',
-		selectHosts='refer',
-		selectGroups='refer',
-		hostids=host_id
-		)
-	if not response:
-		print("Maintenance not found in Zabbix")
-		sys.exit(42)
-	else:
-		result = response[0]["maintenanceid"]
-		return result
+    response = zapi.maintenance.get(
+        output='extend',
+        selectHosts='refer',
+        selectGroups='refer',
+        hostids=host_id
+        )
+    if not response:
+        print("Maintenance not found in Zabbix")
+        sys.exit(42)
+    else:
+        result = response[0]["maintenanceid"]
+        return result
 
 def delete_maintenance(maintenance_id):
-	response = zapi.maintenance.delete(maintenance_id,)
-	error = response.get('error')
-	if not response:
-		print("Maintenance not found in Zabbix")
-		sys.exit(42)
-	else:
-		result = response["maintenanceids"]
-		return result
+    response = zapi.maintenance.delete(maintenance_id,)
+    error = response.get('error')
+    if not response:
+        print("Maintenance not found in Zabbix")
+        sys.exit(42)
+    else:
+        result = response["maintenanceids"]
+        return result
 
 def add_maintenance(host_id,duration,fqdn):
-	start = int(time.time()) - 300 # start maintenance 5 minutes before now to avoid overlapping
-	end = start + int(duration) + 300
-	response = zapi.maintenance.create(
-		groupids = ["5"],
-		hostids = [host_id],
-		name = "Scripted Maintenance: %s" % fqdn,
-		maintenance_type = 0,
-		description = "zbx scripted",
-		active_since = start,
-		active_till = end,
-		timeperiods = [{"timeperiod_type":0,"start_date":start,"period":duration}],
-		)
-	return response
+    start = int(time.time()) - 300 # start maintenance 5 minutes before now to avoid overlapping
+    end = start + int(duration) + 300
+    response = zapi.maintenance.create(
+        groupids = ["5"],
+        hostids = [host_id],
+        name = "Scripted Maintenance: %s" % fqdn,
+        maintenance_type = 0,
+        description = "zbx scripted",
+        active_since = start,
+        active_till = end,
+        timeperiods = [{"timeperiod_type":0,"start_date":start,"period":duration}],
+        )
+    return response
 
 ##########################################################################
 # Maintenance sub command
@@ -149,56 +152,56 @@ def add_maintenance(host_id,duration,fqdn):
 
 @maintenance.command()
 def list():
-	"""List all maintenance currently set in the zabbix server"""
-	response = zapi.maintenance.get(
-    	output='extend',
-    	selectGroups='extend',
-    	selectTimeperiods='extend',
-    	)
-	TableauMaintenance = []
-	for m in response:
-		TableauMaintenance.append([m["name"],m["description"],to_date(m["active_since"]),to_date(m["active_till"])])
-	Header = [Bold+"NAME", "DESCRIPTION", "ACTIVE SINCE", "ACTIVE UNTIL"+UnBold]
-	print(tabulate(TableauMaintenance,headers=Header,tablefmt="plain"))
+    """List all maintenance currently set in the zabbix server"""
+    response = zapi.maintenance.get(
+        output='extend',
+        selectGroups='extend',
+        selectTimeperiods='extend',
+        )
+    TableauMaintenance = []
+    for m in response:
+        TableauMaintenance.append([m["name"],m["description"],to_date(m["active_since"]),to_date(m["active_till"])])
+    Header = [Bold+"NAME", "DESCRIPTION", "ACTIVE SINCE", "ACTIVE UNTIL"+UnBold]
+    print(tabulate(TableauMaintenance,headers=Header,tablefmt="plain"))
 
 @maintenance.command()
 @click.argument('fqdn')
 @click.argument('duration', default = 3600)
 def add(fqdn,duration):
-	"""add a maintenance for the host specified """
-	host_id = get_host_id(fqdn)
-	click.echo('Adding maintenance for %s during %s secondes' % (fqdn, duration))
-	response = add_maintenance(host_id,duration,fqdn)
+    """add a maintenance for the host specified """
+    host_id = get_host_id(fqdn)
+    click.echo('Adding maintenance for %s during %s secondes' % (fqdn, duration))
+    response = add_maintenance(host_id,duration,fqdn)
 
 @maintenance.command()
 @click.argument('fqdn')
 def remove(fqdn):
-	"""Remove a maintenance for the host specified """
-	host_id = get_host_id(fqdn)
-	maintenance_id = get_maintenance_id(host_id)
-	response = delete_maintenance(maintenance_id)
-	click.echo('Maintenance id %s for %s removed' % (response, fqdn))
-	#import pdb; pdb.set_trace()
+    """Remove a maintenance for the host specified """
+    host_id = get_host_id(fqdn)
+    maintenance_id = get_maintenance_id(host_id)
+    response = delete_maintenance(maintenance_id)
+    click.echo('Maintenance id %s for %s removed' % (response, fqdn))
+    #import pdb; pdb.set_trace()
 
 @maintenance.command()
 def gc():
-	"""maintenance garbage collector : remove all expired maintenance"""
-	response = zapi.maintenance.get(
-	output='extend',
-	selectGroups='extend',
-	selectTimeperiods='extend',
+    """maintenance garbage collector : remove all expired maintenance"""
+    response = zapi.maintenance.get(
+    output='extend',
+    selectGroups='extend',
+    selectTimeperiods='extend',
      )
-	to_remove = []
-	for maintenance in response:
-		now = int(time.time())
-		if int(maintenance['active_till']) < now:
-			to_remove.append(maintenance)
-	if len(to_remove) != 0:
-		for m in to_remove:
-			print("Removing expired %s" % m['name'])
-			delete_maintenance(m['maintenanceid'])
-	else:
-		print("No expired maintenance to remove")
+    to_remove = []
+    for maintenance in response:
+        now = int(time.time())
+        if int(maintenance['active_till']) < now:
+            to_remove.append(maintenance)
+    if len(to_remove) != 0:
+        for m in to_remove:
+            print("Removing expired %s" % m['name'])
+            delete_maintenance(m['maintenanceid'])
+    else:
+        print("No expired maintenance to remove")
 
 ##########################################################################
 # zbx general COMMANDS
@@ -208,98 +211,98 @@ def gc():
 @click.argument('fqdn')
 @click.option('--add/--remove',default=None, required=True,help='add or remove a Host of the zabbix server')
 def host(add,fqdn):
-	"""Add or remove a linux host of the zabbix server """
-	if add:
-		template_os_linux = get_tempate_id("Template OS Linux")
-		dns_data = socket.gethostbyname_ex(fqdn)
-		ip = dns_data[2][0]
-		response = zapi.host.create(
-			host=fqdn,
-			interfaces={"type":1,"main":1,"useip":0,"ip":ip,"dns":fqdn,"port":"10050"},
-			groups={"groupid":"5"},
-			templates={"templateid":template_os_linux}
-			)
-		click.echo('%s id %s is added with basic linux template' % (fqdn, response["hostids"][0]))
-	else:
-		host_id = get_host_id(fqdn)
-		response = zapi.host.delete(host_id)
-		click.echo('%s id %s is removed of the zabbix server' % (fqdn, response["hostids"][0]))
+    """Add or remove a linux host of the zabbix server """
+    if add:
+        template_os_linux = get_tempate_id("Template OS Linux")
+        dns_data = socket.gethostbyname_ex(fqdn)
+        ip = dns_data[2][0]
+        response = zapi.host.create(
+            host=fqdn,
+            interfaces={"type":1,"main":1,"useip":0,"ip":ip,"dns":fqdn,"port":"10050"},
+            groups={"groupid":"5"},
+            templates={"templateid":template_os_linux}
+            )
+        click.echo('%s id %s is added with basic linux template' % (fqdn, response["hostids"][0]))
+    else:
+        host_id = get_host_id(fqdn)
+        response = zapi.host.delete(host_id)
+        click.echo('%s id %s is removed of the zabbix server' % (fqdn, response["hostids"][0]))
 
 @zabbix.command()
 @click.argument('fqdn')
 @click.option('--enable/--disable',default=None, required=True,help='enable or disable Host monitoring')
 def monitor(enable,fqdn):
-	"""Enable or Disable monitoring for the host specified"""
-	host_id = get_host_id(fqdn)
-	# enable is status = 0
-	if enable:
-		response = zapi.host.update(
-			hostid=host_id,
-			status=0
-			)
-		click.echo('%s is monitored now' % fqdn)
-	else:
-		response = zapi.host.update(
-			hostid=host_id,
-			status=1
-			)
-		click.echo('%s is not monitored now' % fqdn)
+    """Enable or Disable monitoring for the host specified"""
+    host_id = get_host_id(fqdn)
+    # enable is status = 0
+    if enable:
+        response = zapi.host.update(
+            hostid=host_id,
+            status=0
+            )
+        click.echo('%s is monitored now' % fqdn)
+    else:
+        response = zapi.host.update(
+            hostid=host_id,
+            status=1
+            )
+        click.echo('%s is not monitored now' % fqdn)
 
 @zabbix.command()
 def unmonitored():
-	"""List all host set in zabbix but not monitored (Disabled)"""
-	TableauUnmonitored =[]
-	unmonitored = zapi.host.get(
-		output='extend',
-		filter={"status":1}
-		)
-	for host in unmonitored:
-		TableauUnmonitored.append([host["name"],"Not monitored"])
-	Header = [Bold+"NAME", "STATUS"+UnBold]
-	print(tabulate(TableauUnmonitored,headers=Header,tablefmt="plain"))
+    """List all host set in zabbix but not monitored (Disabled)"""
+    TableauUnmonitored =[]
+    unmonitored = zapi.host.get(
+        output='extend',
+        filter={"status":1}
+        )
+    for host in unmonitored:
+        TableauUnmonitored.append([host["name"],"Not monitored"])
+    Header = [Bold+"NAME", "STATUS"+UnBold]
+    print(tabulate(TableauUnmonitored,headers=Header,tablefmt="plain"))
 
 @zabbix.command()
 def alerts():
-	"""List the last 200 alerts with all that in "warning" or supperior red if no maintenance is configured or acknowledgement envoy"""
-	TableauAlerte =[]
-	triggers = zapi.trigger.get(limite=200,
-		selectLastEvent='extend',
-		selectGroups='extend',
-		selectHosts='extend',
-		monitored=1,
-		skipDependent=1,
-		output='extend',
-		expandDescription=1,
-		expandData='host',
-		sortfield='lastchange',
-		sortorder='DESC',
-		filter={"priority":[2,3,4,5],"value":1}
-		)
+    """List the last 200 alerts with all that in "warning" or supperior red if no maintenance is configured or acknowledgement envoy"""
+    TableauAlerte =[]
+    triggers = zapi.trigger.get(limite=200,
+        selectLastEvent='extend',
+        selectGroups='extend',
+        selectHosts='extend',
+        monitored=1,
+        skipDependent=1,
+        output='extend',
+        expandDescription=1,
+        expandData='host',
+        sortfield='lastchange',
+        sortorder='DESC',
+        filter={"priority":[2,3,4,5],"value":1}
+        )
 
-	for t in triggers:
-		EventId = '-'
-		ack = '-'
-		Maintenance = '-'
-		LastDate = datetime.fromtimestamp( int(t['lastchange']) ).strftime('%Y-%m-%d %H:%M:%S')
-		if t['lastEvent']:
-			EventId = t['lastEvent']['eventid']
-			event = zapi.event.get(eventids=EventId,
-				select_acknowledges='extend',
-				output='extend')
-			if event[0]['acknowledges']:
-				ack_by = event[0]["acknowledges"][0]["alias"]
-				CleanAckDescription = re.sub('----\[BULK ACKNOWLEDGE\]----', r'', event[0]['acknowledges'][0]['message'] )
-				ack = ack_by+ " " + CleanAckDescription
-			if t["hosts"][0]["maintenance_status"] == "1":
-				Maintenance = "Main.."
-		if int(t['priority']) >= 3 and Maintenance != "Main.." and not event[0]['acknowledges'] :
-			Alerte = [eLIGHT_RED+t['hosts'][0]['name'],LastDate,EventId,t["description"],Maintenance,ack+ eCOLOR_NONE]
-		else:
-			Alerte = [t['hosts'][0]['name'],LastDate,EventId,t["description"],Maintenance,ack]
-		TableauAlerte.append(Alerte)
-	Header = [Bold+"Host","Last Event","Event Id","Description","Maintenance","Acknowledge"+UnBold]
-	print(tabulate(TableauAlerte,headers=Header,tablefmt="plain"))
+    for t in triggers:
+        EventId = '-'
+        ack = '-'
+        Maintenance = '-'
+        LastDate = datetime.fromtimestamp( int(t['lastchange']) ).strftime('%Y-%m-%d %H:%M:%S')
+        if t['lastEvent']:
+            EventId = t['lastEvent']['eventid']
+            event = zapi.event.get(eventids=EventId,
+                select_acknowledges='extend',
+                output='extend')
+            if event[0]['acknowledges']:
+                ack_by = event[0]["acknowledges"][0]["alias"]
+                CleanAckDescription = re.sub('----\[BULK ACKNOWLEDGE\]----', r'', event[0]['acknowledges'][0]['message'] )
+                ack = ack_by+ " " + CleanAckDescription
+            if t["hosts"][0]["maintenance_status"] == "1":
+                Maintenance = "Main.."
+        if int(t['priority']) >= 3 and Maintenance != "Main.." and not event[0]['acknowledges'] :
+            Alerte = [eLIGHT_RED+t['hosts'][0]['name'],LastDate,EventId,t["description"],Maintenance,ack+ eCOLOR_NONE]
+        else:
+            Alerte = [t['hosts'][0]['name'],LastDate,EventId,t["description"],Maintenance,ack]
+        TableauAlerte.append(Alerte)
+    Header = [Bold+"Host","Last Event","Event Id","Description","Maintenance","Acknowledge"+UnBold]
+    print(tabulate(TableauAlerte,headers=Header,tablefmt="plain"))
 
 # MAIN
 if __name__ == '__main__':
-	zabbix()
+    zabbix()
