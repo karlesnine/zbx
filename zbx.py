bd2be56173a4f10ee2dccc9411b1b15cf13daacb
Charles-Christian Croix <charles-christian.croix@blablacar.com>
2016-08-12 15:25:36 +0200

    bump version 2.0.0

--- zbx.py
+++ zbx.py
@@ -64,7 +64,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(version='1.0.0')
+@click.version_option(version='2.0.0')
 def zabbix():
     """Zabbix user command line tools."""
     pass
