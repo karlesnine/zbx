a2e169511be4e31f6861a9116ce46c80a8eb3752
Charles-Christian Croix <charles-christian.croix@blablacar.com>
2017-03-02 16:55:44 +0100

    CleanUp

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Install,"""
+# zbx setup.py
+from setuptools import setup
+
+setup(name='zbx',
+      version='3.0.0',
+      author='karles',
+      maintainer='karles',
+      author_email='github+zbx@karlesnine.com',
+      maintainer_email='github+zbx@karlesnine.com',
+      url='https://github.com/karlesnine/zbx',
+      description='cli for zabbix',
+      long_description='cli tool for zabbix user',
+      license='GPL V3',
+      plateformes='ALL',
+      entry_points={
+          'console_scripts': [
+              'zbx = zbx:zabbix'
+          ],
+      },
+      extras_require={
+          ':python_version=="2.7"': ['click>=6', 'requests>=2.11.0', 'logging>=0.4.9.6', 'configparser>=3.5.0'],
+          ':python_version=="3.5"': ['click>=6.6', 'requests>=2.11.0', 'configparser2>=4']
+      }
+      )
